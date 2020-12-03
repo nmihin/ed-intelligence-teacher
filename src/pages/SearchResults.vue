2f84ee1b516661515c0dea2c9f4fba89362f7cef
nmihin <nmihin@gmail.com>
2020-12-03 20:32:04 +0100

    - add feedback form added to student options

--- src/pages/SearchResults.vue
+++ src/pages/SearchResults.vue
@@ -1,6 +1,129 @@
 <template>
   <!-- Main Content -->
   <div class="main-content">
+  <!-- START ADD FEEDBACK LIST MODAL -->
+    <md-dialog :md-active.sync="addFeedbackModal" class="modal-window feedback-list">
+        <h2 class="modal-title">Add Feedback for {{feedbackName}}</h2>
+        <div class="modal-content">
+          <el-form :model="formAddFeedback" :rules="formAddFeedback.rules" ref="formAddFeedback">
+            <div class="row">
+              <el-form-item label="Incident Date" class="col-12 col-md-6">
+                <i class="icon icon-box-plan"></i>
+                <el-date-picker 
+                  @change="updateForm('incidentDate', formAddFeedback.incidentDate)"
+                  prop="birthDate" 
+                  v-model="formAddFeedback.incidentDate" 
+                  type="date" 
+                  format="dd-MM-yyyy" 
+                  value-format="yyyy-MM-dd"  
+                  placeholder="Pick a date">
+              </el-date-picker>
+              </el-form-item>
+              <el-form-item label="School Response" class="col-12 col-md-6">
+                <el-select @change="updateForm('schoolResponse', formAddFeedback.schoolResponse)" v-model="formAddFeedback.schoolResponse" placeholder="School Response">
+                  <el-option v-for="pre in formAddFeedbackOptions.schoolResponseOptions"
+                            :key="pre.value"
+                            :label="pre.label"
+                            :value="pre.value">
+                  </el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="Support Services Recieved" class="col-12 col-md-6">
+                <el-select @change="updateForm('supportServicesRecieved', formAddFeedback.supportServicesRecieved)" v-model="formAddFeedback.supportServicesRecieved" placeholder="Support Services Recieved?">
+                  <el-option v-for="pre in formAddFeedbackOptions.supportServicesRecievedOptions"
+                            :key="pre.value"
+                            :label="pre.label"
+                            :value="pre.value">
+                  </el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="Supporting Document" class="col-12 col-md-6">
+              <el-upload
+                action="https://educationalschool.iteg.com.np/files"
+                @change="updateForm('supportingDocument', formAddFeedback.supportingDocument)" v-model="formAddFeedback.supportingDocument"
+                multiple>
+                <el-button class="button medium ed-btn__primary">
+                    Choose A File
+                </el-button> 
+              </el-upload>
+              </el-form-item>
+            </div>
+            <div class="row">
+              <h3>Incident Comments</h3>
+              <el-form-item label="Teacher Feedback" class="col-12">
+                <el-input type="textarea" @input="updateForm('teacherFeedback', formAddFeedback.teacherFeedback)"  v-model="formAddFeedback.teacherFeedback" prop="teacherFeedback"  placeholder="Teacher Feedback"></el-input>
+              </el-form-item>
+              <el-form-item label="Parent Feedback" class="col-12">
+                <el-input type="textarea" @input="updateForm('parentFeedback', formAddFeedback.parentFeedback)"  v-model="formAddFeedback.parentFeedback" prop="parentFeedback"  placeholder="Parent Feedback"></el-input>
+              </el-form-item>
+              <el-form-item label="Specialist Feedback" class="col-12">
+                <el-input type="textarea" @input="updateForm('specialistFeedback', formAddFeedback.specialistFeedback)"  v-model="formAddFeedback.specialistFeedback" prop="specialistFeedback"  placeholder="Specialist Feedback"></el-input>
+              </el-form-item>
+              <el-form-item label="Action Outcomes" class="col-12">
+                <el-input type="textarea" @input="updateForm('actionOutcomes', formAddFeedback.actionOutcomes)"  v-model="formAddFeedback.actionOutcomes" prop="actionOutcomes"  placeholder="Specialist Feedback"></el-input>
+              </el-form-item>
+              <el-form-item label="Action Date" class="col-12">
+                <i class="icon icon-box-plan"></i>
+                <el-date-picker 
+                  @change="updateForm('actionDate', formAddFeedback.actionDate)"
+                  prop="birthDate" 
+                  v-model="formAddFeedback.actionDate" 
+                  type="date" 
+                  format="dd-MM-yyyy" 
+                  value-format="yyyy-MM-dd"  
+                  placeholder="Pick a date">
+              </el-date-picker>
+              </el-form-item>
+            </div>
+            <div class="row">
+              <el-form-item label="Incident Status" prop="incidentStatus" class="col-12">
+                <el-radio-group @change="updateForm('incidentStatus', formAddFeedback.incidentStatus)" v-model="formAddFeedback.incidentStatus">
+                  <el-radio label="Pending">Pending</el-radio>
+                  <el-radio label="Closed">Closed</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </div>
+          </el-form>
+        </div>
+        <div class="modal-footer">
+            <button class="button medium ed-btn__primary" @click="validateAddFeedback()">
+              Add
+            </button>
+            <button class="button medium ed-btn__tertiary" @click="addFeedbackModal = false">
+              Cancel
+            </button>
+        </div>
+    </md-dialog>
+  <!-- END ADD FEEDBACK LIST MODAL -->  
+  <!-- START FEEDBACK LIST MODAL -->
+    <md-dialog :md-active.sync="feedbackModal" class="modal-window filter-modal feedback-list">
+        <h2 class="modal-title">Feedback List of {{feedbackName}}</h2>
+        <div class="modal-content">
+          <el-table
+            stripe
+            ref="singleTable"
+            :data="feedback"
+            highlight-current-row
+            style="width: 100%">
+              <el-table-column sortable property="id" label="SN" width="60"></el-table-column>
+              <el-table-column sortable property="homeroom" label="Home Room"></el-table-column>
+              <el-table-column sortable property="period" label="Period"></el-table-column>
+              <el-table-column sortable property="type" label="Feedback Type"></el-table-column>
+              <el-table-column sortable property="occureddate" label="Occured Date"></el-table-column>
+              <el-table-column sortable property="status" label="Status"></el-table-column>
+              <el-table-column sortable property="action" label="Action"></el-table-column>
+          </el-table>
+          <div v-if="busy" class="preloader">
+            <span><img src="../assets/images/preloader.gif" /> Loading...</span>
+          </div>
+        </div>
+        <div class="modal-footer">
+            <button class="button medium ed-btn__tertiary" @click="feedbackModal = false">
+              Close
+            </button>
+        </div>
+    </md-dialog>
+  <!-- END FEEDBACK LIST MODAL -->    
     <div class="container-fluid search-results">
       <div class="row">
           <div class="col-12 col-sm-6 col-md-8">
@@ -44,7 +167,7 @@
                       </a>
                       <a class="student-list-preview">
                         <el-tooltip class="item" effect="dark" content="Feedback List" placement="top">
-                          <i @click="feedBackList(post.sn)" class="icon icon-lesson"></i>
+                          <i @click="feedBackList(post.sn,post.name,post.surname)" class="icon icon-lesson"></i>
                         </el-tooltip>
                       </a>
                       <a slot="reference" class="student-list-preview">
@@ -58,17 +181,17 @@
                         trigger="hover">
                         <a class="student-list-preview">
                           <el-tooltip class="item" effect="dark" content="Add Feedback" placement="top">
-                            <i @click="addFeedback(scope.row.sn)" class="icon icon-follow"></i>
+                            <i @click="addFeedback(post.sn)" class="icon icon-follow"></i>
                           </el-tooltip> 
                         </a>
                         <a class="student-list-preview">
                           <el-tooltip class="item" effect="dark" content="Add Withdrawal" placement="top">
-                            <i @click="addWithdrawal(scope.row.sn)" class="icon icon-exit"></i>
+                            <i @click="addWithdrawal(post.sn)" class="icon icon-exit"></i>
                           </el-tooltip>
                         </a>
                         <a class="student-list-preview">
                           <el-tooltip class="item" effect="dark" content="Edit Profile" placement="top">
-                            <i @click="editProfile(scope.row.sn)" class="icon icon-edit"></i>
+                            <i @click="editProfile(post.sn)" class="icon icon-edit"></i>
                           </el-tooltip>
                         </a>
                         <a slot="reference" class="student-list-edit">
@@ -108,8 +231,12 @@ export default {
       searchQuery:"",
       searchName:"",
       posts:[],
+      feedbackModal:false,
+      feedbackName: "",
+      addFeedbackModal: false,
+      feedback: [],
       searchTerm: "name",
-       searchByOptions: [ {
+      searchByOptions: [ {
           value: "name",
           label: 'First Name'
         }, {
@@ -130,7 +257,104 @@ export default {
           value: "guardian",
           label: 'Guardian Name'
         }
-        ],
+      ],
+       formAddFeedback: [{
+         incidentDate:"",
+         schoolResponse:"",
+         supportServicesRecieved:"",
+         supportingDocument:[],
+         teacherFeedback:"",
+         parentFeedback:"",
+         specialistFeedback:"",
+         actionOutcomes:"",
+         actionDate:"",
+         incidentStatus:"",
+         rules: {
+          incidentDate: [
+            {
+              required: true,
+              message: "Incident Date Required!",
+              trigger: "blur",
+            }
+          ],
+          schoolResponse: [
+            {
+              required: true,
+              message: "Incident Date Required!",
+              trigger: "blur",
+            }
+          ],
+          supportServicesRecieved: [
+            {
+              required: true,
+              message: "Incident Date Required!",
+              trigger: "blur",
+            }
+          ],
+          supportingDocument: [
+            {
+              required: true,
+              message: "Incident Date Required!",
+              trigger: "blur",
+            }
+          ],
+          teacherFeedback: [
+            {
+              required: true,
+              message: "Incident Date Required!",
+              trigger: "blur",
+            }
+          ],
+          parentFeedback: [
+            {
+              required: true,
+              message: "Incident Date Required!",
+              trigger: "blur",
+            }
+          ],
+          specialistFeedback: [
+            {
+              required: true,
+              message: "Incident Date Required!",
+              trigger: "blur",
+            }
+          ],
+          actionOutcomes: [
+            {
+              required: true,
+              message: "Incident Date Required!",
+              trigger: "blur",
+            }
+          ],
+          actionDate: [
+            {
+              required: true,
+              message: "Incident Date Required!",
+              trigger: "blur",
+            }
+          ],
+          incidentStatus: [
+            {
+              required: true,
+              message: "Incident Date Required!",
+              trigger: "blur",
+            }
+          ]
+         }
+       }],
+       formAddFeedbackOptions: {
+          schoolResponseOptions: [
+            { value: "familyphone", label: "Family Phone Call" },
+            { value: "familymeeting", label: "Family Meeting" },
+            { value: "suspended", label: "Suspended" },
+            { value: "expelled", label: "Expelled" }
+          ],
+          supportServicesRecievedOptions: [
+            { value: "familymeeting", label: "Family Meeting" },
+            { value: "inclassplan", label: "In Class Behavioral Plan" },
+            { value: "outclassplan", label: "Out Class Behavioral Plan" }
+          ]
+       }
     }),
     // METHODS
     methods: {
@@ -182,8 +406,61 @@ export default {
       loadStudentlistStorage() {
         return JSON.parse(localStorage.getItem("studentListStorageJSONData"));
       },
+      // LOCALSTORAGE
+      loadFeedbackListStorage() {
+        return JSON.parse(localStorage.getItem("feedbackListJSONData"));
+      },
+      feedBackList(id,name,surname){
+        this.feedbackName = name+" "+surname;
+
+        const feedbackListStorage = this.loadFeedbackListStorage();
+        if(feedbackListStorage){
+            const fdb = feedbackListStorage.filter(function(feedback) {
+              return feedback.sn === id;
+            });
+
+            this.feedback = fdb[0].feedback;
+        }
+        else{
+          this.busy = true;
+          this.axios.get("https://raw.githubusercontent.com/nmihin/ed-intelligence-teacher__deploy/master/feedback-list.json").then((response) => {
+
+                  const fdb = response.data.filter(function(feedback) {
+                    return feedback.sn === id;
+                  });
+
+                  this.feedback = fdb[0].feedback;
+
+                  // UPDATE STORAGE
+                  localStorage.setItem("feedbackListJSONData",JSON.stringify(response.data));
+
+                  this.busy = false;
+            }).catch((error) => error.response.data)
+          this.busy = false;
+        }
+
+        this.feedbackModal = true;
+      },
       viewProfile(id){
         this.$router.push({path:'/student-details/'+id})
+      },
+      editProfile(id){
+        this.$router.push({path:'/add-student/'+id})
+      },
+      addFeedback(id,name,surname){
+        this.feedbackName = name+" "+surname;
+
+        this.addFeedbackModal = true;
+      },
+      validateAddFeedback(){
+        return new Promise((resolve) => {
+          this.$refs.formAddFeedback.validate((valid) => {
+            this.$emit("on-validate", valid, this.model);
+            resolve(valid);
+            if(valid)
+              console.log("valid")
+          });
+        });
       },
       updateSearchTerm() {
 
